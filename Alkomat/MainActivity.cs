e246787aa035ecff83fe842f9d3dea744ee6d27d
MSobieraj <mateusz.sobieraj91@gmail.com>
2015-08-02 21:55:15 +0200

    zmiany

--- Alkomat/MainActivity.cs
+++ Alkomat/MainActivity.cs
@@ -13,60 +13,58 @@ namespace Alkomat
     [Activity(Label = "Alkomat", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        
+        Button buttonDalej;
+        RadioButton radio_kob;
+        RadioButton radio_mez;
+        EditText editText1;
+        EditText editText2;
+        EditText editText3;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            double tbw = 0;
-            double wzrost = 0;
-            double waga = 0;
-            double wiek = 0;
-
-            tbw = Zmienne.tbw;
-            wzrost = Zmienne.wzrost;
-            waga = Zmienne.waga;
-            wiek = Zmienne.wiek;
+            
+            Zmienne.tbw = 0;
+             Zmienne.wzrost = 0;
+             Zmienne.waga = 0;
+             Zmienne.wiek = 0;
             
             SetContentView(Resource.Layout.Main);
 
-            Button buttonDalej = FindViewById<Button>(Resource.Id.buttonDalej);   
-            RadioButton radio_kob = FindViewById<RadioButton>(Resource.Id.radioButton1);
-            RadioButton radio_mez = FindViewById<RadioButton>(Resource.Id.radioButton2);
-            EditText editText1 = FindViewById<EditText>(Resource.Id.editText1);
-            EditText editText2 = FindViewById<EditText>(Resource.Id.editText2);
-            EditText editText3 = FindViewById<EditText>(Resource.Id.editText3);
+             buttonDalej = FindViewById<Button>(Resource.Id.buttonDalej);   
+             radio_kob = FindViewById<RadioButton>(Resource.Id.radioButton1);
+             radio_mez = FindViewById<RadioButton>(Resource.Id.radioButton2);
+             editText1 = FindViewById<EditText>(Resource.Id.editText1);
+             editText2 = FindViewById<EditText>(Resource.Id.editText2);
+             editText3 = FindViewById<EditText>(Resource.Id.editText3);
 
             buttonDalej.Click += ButtonDalej_Click;   
         }
-        /*
-        public double zczytajPola(EditText editText1, EditText editText2, EditText editText3, RadioButton radio_kob, RadioButton radio_mez)
+
+        public double zczytajPola()
         {
             //wyczytałem, że dane z edittextów wpierw trzeba do stringa a dopiero potem do double.
 
-            string dane1 = Convert.ToString(editText1);
-            string dane2 = Convert.ToString(editText2);
-            string dane3 = Convert.ToString(editText3);
+            //string dane1 = Convert.ToString(editText1);
+            //string dane2 = Convert.ToString(editText2);
+            //string dane3 = Convert.ToString(editText3);
 
-            wzrost = Convert.ToDouble(dane1);
-            waga = Convert.ToDouble(dane2);
-            wiek = Convert.ToDouble(dane3);
+            Zmienne.wzrost = Convert.ToDouble(editText1);
+            Zmienne.waga = Convert.ToDouble(editText2);
+            Zmienne.wiek = Convert.ToDouble(editText3);
 
-              
-            if (radio_kob.Checked) {
-                tbw = -2.097 + (0.1069 * Zmienne.wzrost) + (0.2466 * Zmienne.waga);
-                }
-            else if (radio_mez.Checked) {
-                tbw = 2.447 - (0.09156 * Zmienne.wiek) + (0.1074 * Zmienne.wzrost) + (0.3362 * Zmienne.waga);
-                }
-            return tbw;
+
+            if (radio_kob.Checked)
+                Zmienne.tbw = -2.097 + (0.1069 * Zmienne.wzrost) + (0.2466 * Zmienne.waga);
+            else if (radio_mez.Checked)
+                Zmienne.tbw = 2.447 - (0.09156 * Zmienne.wiek) + (0.1074 * Zmienne.wzrost) + (0.3362 * Zmienne.waga);
+            return Zmienne.tbw;
         }
-        */
+
 
         private void ButtonDalej_Click(object sender, EventArgs e)
         {
-            //zczytajPola(tbw);
+            zczytajPola();
             Intent intent = new Intent(this, typeof(ObliczPromile));
             this.StartActivity(intent);
         }
