495f34b513491f2db83f170461a0f91f45dae557
MSobieraj <mateusz.sobieraj91@gmail.com>
2015-08-04 01:27:53 +0200

    dziala godzinka, dodalem funkcje ToTime(double x) ktora zwraca stringa, zmienialem jeszce 0.02 promila na 0.20

--- Alkomat/Wynik.cs
+++ Alkomat/Wynik.cs
@@ -28,8 +28,6 @@ namespace Alkomat
             SetContentView(Resource.Layout.Wynik);
 
             // Create your application here
-            DateTime godzinaDopuszczalna;
-            DateTime godzinaWytrzezwienia;
 
             Alcolator.Wyliczamy();
             Alcolator.KiedyTrzezwy();
@@ -40,37 +38,58 @@ namespace Alkomat
             textView_AktulnePromile = FindViewById<TextView>(Resource.Id.textView_AktulnePromile);
             textView_AktulnePromile.Text = Math.Round(Zmienne.wynik, 2).ToString();
             textView_AktulnePromile.Text = textView_AktulnePromile.Text + '‰';
-
-            Zmienne.godzinaWytrzezwienia = DateTime.Now;
-            Zmienne.godzinaWytrzezwienia.AddHours(Zmienne.wynik2);
+            
             double pob = Zmienne.wynik2;
-            if (pob>24)
-            {
-                double a = pob - 24;
-                pob = a;
-            }
-
+            
+            //if (pob>24)
+            //{
+            //    double a = pob - 24;
+            //    pob = a;
+            //}
+            //Math.Round(pob, 2).ToString();
             TextView_WytrzezwiejeszGodzina = FindViewById<TextView>(Resource.Id.TextView_WytrzezwiejeszGodzina);
-            TextView_WytrzezwiejeszGodzina.Text = Zmienne.godzinaWytrzezwienia.ToShortTimeString();
+            TextView_WytrzezwiejeszGodzina.Text = ToTime(pob);
             TextView_WytrzezwiejeszGodzina.Text = TextView_WytrzezwiejeszGodzina.Text.Replace(',', ':');
             TextView_WytrzezwiejeszGodzina.Text = "o godz: " + TextView_WytrzezwiejeszGodzina.Text;
 
             TextView_DopuszczalneStezenieGodzina = FindViewById<TextView>(Resource.Id.TextView_DopuszczalneStezenieGodzina);
 
             double pob2 = pob - 1;
-            if (pob2>24)
-            {
-                double b = pob2 - 24;
-                pob2 = b;
-            }
-            Zmienne.godzinaDopuszczalna = DateTime.Now;
-            Zmienne.godzinaDopuszczalna.AddHours(pob2);
+            //if (pob2>24)
+            //{
+            //    double b = pob2 - 24;
+            //    pob2 = b;
+            //}
 
-            TextView_DopuszczalneStezenieGodzina.Text = Zmienne.godzinaDopuszczalna.ToShortTimeString();
+            TextView_DopuszczalneStezenieGodzina.Text = ToTime(pob2);
             TextView_DopuszczalneStezenieGodzina.Text = TextView_DopuszczalneStezenieGodzina.Text.Replace(',', ':');
             TextView_DopuszczalneStezenieGodzina.Text = "o godz: " + TextView_DopuszczalneStezenieGodzina.Text;
 
             buttonWynik.Click += buttonWynik_Click;
+        }
+
+        private string ToTime(double pob)
+        {
+
+            string godzinka = Math.Round(pob, 2).ToString();
+            List<string> temp = new List<string>();
+            temp.AddRange(godzinka.Split(','));
+           // string minuty = godzinka.Substring(godzinka.Length - 2, godzinka.Length);
+            int hour = Convert.ToInt16(temp[0]); //((int)Math.Floor(pob));
+            int minutes = Convert.ToInt16(temp[1]);
+            if (minutes > 60)
+                hour++;
+            minutes = minutes % 60;
+            
+
+            hour = hour % 24;
+            godzinka = "";
+            godzinka = hour.ToString() + ":";
+            if (minutes < 10)
+                godzinka += "0" + minutes.ToString();
+            else
+                godzinka += minutes.ToString();
+            return godzinka;
         }
 
         private void buttonWynik_Click(object sender, EventArgs e)
