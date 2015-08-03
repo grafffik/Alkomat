1f17c53c2deeedf1824d4e1a585ae9ced4d24f86
grafffik <graffffik@gmail.com>
2015-08-03 10:40:50 +0200

    podajGodzine.cs breakpoint w Alcolator.cs

--- Alkomat/podajGodzine.cs
+++ Alkomat/podajGodzine.cs
@@ -22,6 +22,7 @@ namespace Alkomat
         double m1;
         double g2;
         double m2;
+        TimePicker zegar;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -45,22 +46,29 @@ namespace Alkomat
             zegar.SetIs24HourView(Java.Lang.Boolean.True);
 
             //pobranie godziny i minut z zegara w apce
-            g2 = Convert.ToDouble(zegar.CurrentHour);
-            m2 = Convert.ToDouble(zegar.CurrentMinute);
+            
             
             //zsumowanie
-            czas2 = g2 + (m2 / 60);
+            
 
             //Zmienne.czasPicia to ile czasu mine³o od skoñczenia melan¿u do chwili obecnej
             //ostatnia zmienna poczebna do wyniku
-            Zmienne.czasPicia = czas1 - czas2;
+            
 
             Button buttonPodajGodzine = FindViewById<Button>(Resource.Id.buttonPodajGodzine);
             buttonPodajGodzine.Click += ButtonPodajGodzine_Click;
+        }
+        public void liczGodzine()
+        {
+            g2 = Convert.ToDouble(zegar.CurrentHour);
+            m2 = Convert.ToDouble(zegar.CurrentMinute);
+            czas2 = g2 + (m2 / 60);
+            Zmienne.czasPicia = czas1 - czas2;
         }        
         
         private void ButtonPodajGodzine_Click(object sender, EventArgs e)
-        {            
+        {
+            liczGodzine();            
             Intent intent = new Intent(this, typeof(Reklama));
             this.StartActivity(intent);
         }        
