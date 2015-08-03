7d7c53fc9db7fdc3fe87f706442c370a963c672b
grafffik <graffffik@gmail.com>
2015-08-03 17:55:40 +0200

    działa dodawanie alkoholi

--- Alkomat/ObliczPromile.cs
+++ Alkomat/ObliczPromile.cs
@@ -22,7 +22,28 @@ namespace Alkomat
         TextView tv4;
         TextView tv5;
         TextView tv6;
-        TextView tv7;    
+        TextView tv7;
+        Button buttonM1;
+        Button buttonM2;
+        Button buttonM3;
+        Button buttonM4;
+        Button buttonM5;
+        Button buttonM6;
+        Button buttonM7;
+        Button buttonP1;
+        Button buttonP2;
+        Button buttonP3;
+        Button buttonP4;
+        Button buttonP5;
+        Button buttonP6;
+        Button buttonP7;
+        int w1;
+        int w2;
+        int w3;
+        int w4;
+        int w5;
+        int w6;
+        int w7;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -40,19 +61,189 @@ namespace Alkomat
             Zmienne.iloscWodkaDuza = 0;
             Zmienne.wspolczynnikA = 0;
 
+            w1 = 0;
+            w2 = 0;
+            w3 = 0;
+            w4 = 0;
+            w5 = 0;
+            w6 = 0;
+            w7 = 0;
+
             Button buttonObliczPromileDalej = FindViewById<Button>(Resource.Id.buttonObliczPromileDalej);
 
-            tv1= FindViewById<TextView>(Resource.Id.tv1);
-            tv2= FindViewById<TextView>(Resource.Id.tv2);
-            tv3= FindViewById<TextView>(Resource.Id.tv3);
-            tv4= FindViewById<TextView>(Resource.Id.tv4);
-            tv5= FindViewById<TextView>(Resource.Id.tv5);
-            tv6= FindViewById<TextView>(Resource.Id.tv6);
-            tv7= FindViewById<TextView>(Resource.Id.tv7);
+            tv1 = FindViewById<TextView>(Resource.Id.tv1);
+            tv2 = FindViewById<TextView>(Resource.Id.tv2);
+            tv3 = FindViewById<TextView>(Resource.Id.tv3);
+            tv4 = FindViewById<TextView>(Resource.Id.tv4);
+            tv5 = FindViewById<TextView>(Resource.Id.tv5);
+            tv6 = FindViewById<TextView>(Resource.Id.tv6);
+            tv7 = FindViewById<TextView>(Resource.Id.tv7);
+
+            buttonM1 = FindViewById<Button>(Resource.Id.buttonM1);
+            buttonM2 = FindViewById<Button>(Resource.Id.buttonM2);
+            buttonM3 = FindViewById<Button>(Resource.Id.buttonM3);
+            buttonM4 = FindViewById<Button>(Resource.Id.buttonM4);
+            buttonM5 = FindViewById<Button>(Resource.Id.buttonM5);
+            buttonM6 = FindViewById<Button>(Resource.Id.buttonM6);
+            buttonM7 = FindViewById<Button>(Resource.Id.buttonM7);
+            buttonP1 = FindViewById<Button>(Resource.Id.buttonP1);
+            buttonP2 = FindViewById<Button>(Resource.Id.buttonP2);
+            buttonP3 = FindViewById<Button>(Resource.Id.buttonP3);
+            buttonP4 = FindViewById<Button>(Resource.Id.buttonP4);
+            buttonP5 = FindViewById<Button>(Resource.Id.buttonP5);
+            buttonP6 = FindViewById<Button>(Resource.Id.buttonP6);
+            buttonP7 = FindViewById<Button>(Resource.Id.buttonP7);
+
+            buttonP1.Click += ButtonP1_Click;
+            buttonP2.Click += ButtonP2_Click;
+            buttonP3.Click += ButtonP3_Click;
+            buttonP4.Click += ButtonP4_Click;
+            buttonP5.Click += ButtonP5_Click;
+            buttonP6.Click += ButtonP6_Click;
+            buttonP7.Click += ButtonP7_Click;
+
+            buttonM1.Click += ButtonM1_Click;
+            buttonM2.Click += ButtonM2_Click;
+            buttonM3.Click += ButtonM3_Click;
+            buttonM4.Click += ButtonM4_Click;
+            buttonM5.Click += ButtonM5_Click;
+            buttonM6.Click += ButtonM6_Click;
+            buttonM7.Click += ButtonM7_Click;
 
             buttonObliczPromileDalej.Click += ButtonObliczPromileDalej_Click;            
         }
+
+        private void ButtonM7_Click(object sender, EventArgs e)
+        {
+            if (w7 > 0)
+            {
+                --w7;
+            }
+            else
+            {
+                w7 = 0;
+            }
+            tv7.Text = Convert.ToString(w7);
+        }
+
+        private void ButtonM6_Click(object sender, EventArgs e)
+        {
+            if (w6 > 0)
+            {
+                --w6;
+            }
+            else
+            {
+                w6 = 0;
+            }
+            tv6.Text = Convert.ToString(w6);
+        }
+
+        private void ButtonM5_Click(object sender, EventArgs e)
+        {
+            if (w5 > 0)
+            {
+                --w5;
+            }
+            else
+            {
+                w5 = 0;
+            }
+            tv5.Text = Convert.ToString(w5);
+        }
+
+        private void ButtonM4_Click(object sender, EventArgs e)
+        {
+            if (w4 > 0)
+            {
+                --w4;
+            }
+            else
+            {
+                w4 = 0;
+            }
+            tv4.Text = Convert.ToString(w4);
+        }
+
+        private void ButtonM3_Click(object sender, EventArgs e)
+        {
+            if (w3 > 0)
+            {
+                --w3;
+            }
+            else
+            {
+                w3 = 0;
+            }
+            tv3.Text = Convert.ToString(w3);
+        }
+
+        private void ButtonM2_Click(object sender, EventArgs e)
+        {
+            if (w2 > 0)
+            {
+                --w2;
+            }
+            else
+            {
+                w2 = 0;
+            }
+            tv2.Text = Convert.ToString(w2);
+        }
+        private void ButtonM1_Click(object sender, EventArgs e)
+        {
+            if (w1 > 0)
+            {
+                --w1;
+            }
+            else
+            {
+                w1 = 0;
+            }
+            tv1.Text = Convert.ToString(w1);
+        }
+        private void ButtonP7_Click(object sender, EventArgs e)
+        {
+            ++w7;
+            tv7.Text = Convert.ToString(w7);
+        }
+
+        private void ButtonP6_Click(object sender, EventArgs e)
+        {
+            ++w6;
+            tv6.Text = Convert.ToString(w6);
+        }
+
+        private void ButtonP5_Click(object sender, EventArgs e)
+        {
+            ++w5;
+            tv5.Text = Convert.ToString(w5);
+        }
+
+        private void ButtonP4_Click(object sender, EventArgs e)
+        {
+            ++w4;
+            tv4.Text = Convert.ToString(w4);
+        }
+
+        private void ButtonP3_Click(object sender, EventArgs e)
+        {
+            ++w3;
+            tv3.Text = Convert.ToString(w3);
+        }       
+
+        private void ButtonP2_Click(object sender, EventArgs e)
+        {
+            ++w2;
+            tv2.Text = Convert.ToString(w2);
+        }
+
+        private void ButtonP1_Click(object sender, EventArgs e) {
+            ++w1;
+            tv1.Text = Convert.ToString(w1);
+        }
         
+
         public void zsumujMelanz()
         {
             Zmienne.iloscDuzePiwo = Convert.ToInt32(tv1.Text);
--- Alkomat/Resources/Resource.Designer.cs
+++ Alkomat/Resources/Resource.Designer.cs
@@ -121,6 +121,9 @@ namespace Alkomat
 			// aapt resource value: 0x7f020004
 			public const int przyciskPlus = 2130837508;
 			
+			// aapt resource value: 0x7f020005
+			public const int tapeta = 2130837509;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -134,32 +137,74 @@ namespace Alkomat
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070023
-			public const int TextViewKtoraGodzina = 2131165219;
+			// aapt resource value: 0x7f07002f
+			public const int TextViewKtoraGodzina = 2131165231;
 			
-			// aapt resource value: 0x7f070027
-			public const int TextView_AktualnyWynik = 2131165223;
+			// aapt resource value: 0x7f070033
+			public const int TextView_AktualnyWynik = 2131165235;
 			
-			// aapt resource value: 0x7f070029
-			public const int TextView_DopuszczalneStezenie = 2131165225;
+			// aapt resource value: 0x7f070035
+			public const int TextView_DopuszczalneStezenie = 2131165237;
 			
-			// aapt resource value: 0x7f07002a
-			public const int TextView_DopuszczalneStezenieGodzina = 2131165226;
+			// aapt resource value: 0x7f070036
+			public const int TextView_DopuszczalneStezenieGodzina = 2131165238;
 			
 			// aapt resource value: 0x7f07000a
 			public const int buttonDalej = 2131165194;
 			
-			// aapt resource value: 0x7f070022
-			public const int buttonObliczPromileDalej = 2131165218;
+			// aapt resource value: 0x7f07000d
+			public const int buttonM1 = 2131165197;
 			
-			// aapt resource value: 0x7f070025
-			public const int buttonPodajGodzine = 2131165221;
+			// aapt resource value: 0x7f070012
+			public const int buttonM2 = 2131165202;
+			
+			// aapt resource value: 0x7f070017
+			public const int buttonM3 = 2131165207;
+			
+			// aapt resource value: 0x7f07001c
+			public const int buttonM4 = 2131165212;
+			
+			// aapt resource value: 0x7f070021
+			public const int buttonM5 = 2131165217;
 			
 			// aapt resource value: 0x7f070026
-			public const int buttonReklama = 2131165222;
+			public const int buttonM6 = 2131165222;
 			
 			// aapt resource value: 0x7f07002b
-			public const int buttonWynik = 2131165227;
+			public const int buttonM7 = 2131165227;
+			
+			// aapt resource value: 0x7f07002e
+			public const int buttonObliczPromileDalej = 2131165230;
+			
+			// aapt resource value: 0x7f07000f
+			public const int buttonP1 = 2131165199;
+			
+			// aapt resource value: 0x7f070014
+			public const int buttonP2 = 2131165204;
+			
+			// aapt resource value: 0x7f070019
+			public const int buttonP3 = 2131165209;
+			
+			// aapt resource value: 0x7f07001e
+			public const int buttonP4 = 2131165214;
+			
+			// aapt resource value: 0x7f070023
+			public const int buttonP5 = 2131165219;
+			
+			// aapt resource value: 0x7f070028
+			public const int buttonP6 = 2131165224;
+			
+			// aapt resource value: 0x7f07002d
+			public const int buttonP7 = 2131165229;
+			
+			// aapt resource value: 0x7f070031
+			public const int buttonPodajGodzine = 2131165233;
+			
+			// aapt resource value: 0x7f070032
+			public const int buttonReklama = 2131165234;
+			
+			// aapt resource value: 0x7f070037
+			public const int buttonWynik = 2131165239;
 			
 			// aapt resource value: 0x7f07000c
 			public const int duzepiwo = 2131165196;
@@ -173,17 +218,11 @@ namespace Alkomat
 			// aapt resource value: 0x7f070009
 			public const int editText3 = 2131165193;
 			
-			// aapt resource value: 0x7f070014
-			public const int lekkiepiwo = 2131165204;
+			// aapt resource value: 0x7f070016
+			public const int lekkiepiwo = 2131165206;
 			
 			// aapt resource value: 0x7f070011
 			public const int malepiwo = 2131165201;
-			
-			// aapt resource value: 0x7f07000d
-			public const int przyciskMinus = 2131165197;
-			
-			// aapt resource value: 0x7f07000f
-			public const int przyciskPlus = 2131165199;
 			
 			// aapt resource value: 0x7f070003
 			public const int radioButton1 = 2131165187;
@@ -194,29 +233,29 @@ namespace Alkomat
 			// aapt resource value: 0x7f070002
 			public const int radioGroup1 = 2131165186;
 			
-			// aapt resource value: 0x7f07001a
-			public const int szampan = 2131165210;
+			// aapt resource value: 0x7f070020
+			public const int szampan = 2131165216;
 			
 			// aapt resource value: 0x7f07000b
 			public const int tableRow1 = 2131165195;
 			
-			// aapt resource value: 0x7f070013
-			public const int tableRow2 = 2131165203;
+			// aapt resource value: 0x7f070015
+			public const int tableRow2 = 2131165205;
 			
 			// aapt resource value: 0x7f070010
 			public const int tableRow3 = 2131165200;
 			
-			// aapt resource value: 0x7f070016
-			public const int tableRow4 = 2131165206;
-			
-			// aapt resource value: 0x7f070019
-			public const int tableRow5 = 2131165209;
-			
-			// aapt resource value: 0x7f07001c
-			public const int tableRow6 = 2131165212;
+			// aapt resource value: 0x7f07001a
+			public const int tableRow4 = 2131165210;
 			
 			// aapt resource value: 0x7f07001f
-			public const int tableRow7 = 2131165215;
+			public const int tableRow5 = 2131165215;
+			
+			// aapt resource value: 0x7f070024
+			public const int tableRow6 = 2131165220;
+			
+			// aapt resource value: 0x7f070029
+			public const int tableRow7 = 2131165225;
 			
 			// aapt resource value: 0x7f070001
 			public const int textView1 = 2131165185;
@@ -230,41 +269,41 @@ namespace Alkomat
 			// aapt resource value: 0x7f070000
 			public const int textView4 = 2131165184;
 			
-			// aapt resource value: 0x7f070028
-			public const int textView_AktulnePromile = 2131165224;
+			// aapt resource value: 0x7f070034
+			public const int textView_AktulnePromile = 2131165236;
 			
-			// aapt resource value: 0x7f070024
-			public const int timePicker1 = 2131165220;
+			// aapt resource value: 0x7f070030
+			public const int timePicker1 = 2131165232;
 			
 			// aapt resource value: 0x7f07000e
 			public const int tv1 = 2131165198;
 			
-			// aapt resource value: 0x7f070012
-			public const int tv2 = 2131165202;
-			
-			// aapt resource value: 0x7f070015
-			public const int tv3 = 2131165205;
+			// aapt resource value: 0x7f070013
+			public const int tv2 = 2131165203;
 			
 			// aapt resource value: 0x7f070018
-			public const int tv4 = 2131165208;
-			
-			// aapt resource value: 0x7f07001b
-			public const int tv5 = 2131165211;
-			
-			// aapt resource value: 0x7f07001e
-			public const int tv6 = 2131165214;
-			
-			// aapt resource value: 0x7f070021
-			public const int tv7 = 2131165217;
-			
-			// aapt resource value: 0x7f070017
-			public const int wino = 2131165207;
-			
-			// aapt resource value: 0x7f070020
-			public const int wodkaDuza = 2131165216;
+			public const int tv3 = 2131165208;
 			
 			// aapt resource value: 0x7f07001d
-			public const int wodkaMala = 2131165213;
+			public const int tv4 = 2131165213;
+			
+			// aapt resource value: 0x7f070022
+			public const int tv5 = 2131165218;
+			
+			// aapt resource value: 0x7f070027
+			public const int tv6 = 2131165223;
+			
+			// aapt resource value: 0x7f07002c
+			public const int tv7 = 2131165228;
+			
+			// aapt resource value: 0x7f07001b
+			public const int wino = 2131165211;
+			
+			// aapt resource value: 0x7f07002a
+			public const int wodkaDuza = 2131165226;
+			
+			// aapt resource value: 0x7f070025
+			public const int wodkaMala = 2131165221;
 			
 			static Id()
 			{
